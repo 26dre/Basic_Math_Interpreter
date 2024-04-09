943bfeafad2a8159cb5504fbc229ce2e6823d649
Andre Khmelnitsky <akhmelni@uci.edu>
2024-04-09 02:07:03 -0700

    added basic tests in the main file, need to refactor code s.t. it operates on imperfect input

--- basic_math_interpreter/src/main.rs
+++ basic_math_interpreter/src/main.rs
@@ -5,6 +5,8 @@ const CLOSING_PAREN: char = ')';
 const OPENING_PAREN: char = '(';
 const PLUS: char = '+';
 const TIMES: char = '*';
+const DIVIDE: char = '/';
+const MINUS: char = '-';
 
 static mut ACCUMULATOR: usize = 0;
 
@@ -16,8 +18,9 @@ fn main() {
     // let x = make_number(&should_work_array, idx);
     // println!("{x}");
 
-    let input_string = String::from("123*124");
-    parse_perfect_input(&input_string);
+    let input_string = String::from("10+20*(10+3)");
+    let ret_val = parse_perfect_input(&input_string);
+    println!("{}", ret_val[0]);
     // let mut x = 100;
     // next(&mut x);
 
@@ -90,40 +93,47 @@ fn match_chars(curr_char: &char, char_to_match: &char) {
     }
 }
 
-fn parse_perfect_input(s: &String) -> Vec<isize> {
-    let ret_vec: Vec<isize> = Vec::new();
+fn parse_perfect_input(s: &String) -> Vec<usize> {
+    let mut ret_vec: Vec<usize> = Vec::new();
 
     let char_vector: Vec<char> = s.chars().collect();
     let mut curr_idx = 0;
-    expression_perfect(&char_vector, &mut curr_idx);
+    let ret_val = expression_perfect(&char_vector, &mut curr_idx);
 
+    ret_vec.push(ret_val);
     ret_vec
 }
 
-fn expression_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) {
-    term_perfect(char_vec, curr_idx);
+fn expression_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) -> usize {
+    let mut val: usize = term_perfect(char_vec, curr_idx);
     while *curr_idx < char_vec.len() && char_vec[*curr_idx] == PLUS {
         println!("Inside while ctrl flow with curr_idx val of {}", *curr_idx);
         next(curr_idx);
-        term_perfect(char_vec, curr_idx);
+        val += term_perfect(char_vec, curr_idx);
     }
+
+    val
 }
 
-fn term_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) {
-    factor_perfect(char_vec, curr_idx);
+fn term_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) -> usize {
+    let mut val: usize = factor_perfect(char_vec, curr_idx);
     while *curr_idx < char_vec.len() && char_vec[*curr_idx] == TIMES {
         next(curr_idx);
-        factor_perfect(char_vec, curr_idx);
+        val *= factor_perfect(char_vec, curr_idx);
     }
+
+    val
 }
-fn factor_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) {
+
+fn factor_perfect(char_vec: &Vec<char>, curr_idx: &mut usize) -> usize {
     if char_vec[*curr_idx].is_numeric() {
-        make_number(char_vec, curr_idx);
+        make_number(char_vec, curr_idx)
     } else if char_vec[*curr_idx] == OPENING_PAREN {
         next(curr_idx);
-        expression_perfect(char_vec, curr_idx);
+        let temp = expression_perfect(char_vec, curr_idx);
         match_chars(&char_vec[*curr_idx], &CLOSING_PAREN);
-        next(curr_idx);
+        temp
+        // next(curr_idx);
         //at this point the function should properly return the value that it has received
         // I will implement this functionality later
     } else {
@@ -143,4 +153,40 @@ fn make_number(char_vec: &Vec<char>, curr_idx: &mut usize) -> usize {
 
     println!("About to return the number {ret_num}");
     ret_num as usize
+}
+
+mod tests {
+    use std::result;
+
+    use crate::parse_perfect_input;
+
+    #[test]
+    fn works() {
+        let test_str = String::from("123+124");
+        let result = parse_perfect_input(&test_str)[0];
+        // println!("Running this test case");
+        assert_eq!(result, 247);
+    }
+    #[test]
+    fn multiplication_test() {
+        let test_str = String::from("10*25");
+        let result = parse_perfect_input(&test_str)[0];
+        // println!("Running")
+        assert_eq!(result, 250)
+    }
+
+    #[test]
+    fn add_and_multiply() {
+        let test_str = String::from("10+25*4");
+        let result = parse_perfect_input(&test_str)[0];
+        assert_eq!(result, 110);
+    }
+    #[test]
+    fn add_and_multiply_and_paren() {
+        let test_str = String::from("10+25*(10+2+(4*4))");
+        let result = parse_perfect_input(&test_str)[0];
+        assert_eq!(result, 710);
+    }
+
+    // fn works2
 }
