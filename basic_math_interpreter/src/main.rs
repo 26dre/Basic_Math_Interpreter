dedea4118eb4482086621093037f939c318223f0
Andre Khmelnitsky <akhmelni@uci.edu>
2024-04-09 23:41:39 -0700

    never mind this is literallye exaclty y we use github

--- basic_math_interpreter/src/main.rs
+++ basic_math_interpreter/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     let input_string = String::from("10+20*(10+3)");
     let ret_val = parse_perfect_input(&input_string);
     println!("{}", ret_val[0]);
-    println!("Hello world motherfuckers\n");
 
 }
 
